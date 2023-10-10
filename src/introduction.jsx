a879efaa4f9c0606732c38706362aea98aedf62d
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-10-10 21:44:30 +0100

    modification du fichier introduction

--- src/introduction.jsx
+++ src/introduction.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Introduction = () => {
   return (
-    <section className="bg-light py-5">
+    <section className="bg-gray-200 py-5">
       <div className="container">
-        <div className="row">
-          <div className="mx-auto text-center">
-            <h2 className="display-4">Bienvenue sur notre plateforme d'inscription en ligne</h2>
-            <p className="lead">
-              Vous êtes sur le point de commencer le processus d'inscription à l'Université d'Abomey-Calavi (UAC).
-              Notre page "Comment ça marche ?" est là pour vous guider à chaque étape du processus, de manière simple et claire.
-              Suivez les étapes ci-dessous pour vous inscrire avec succès.
-            </p>
-          </div>
+        <div className="mx-auto text-center">
+          <h2 className="text-4xl font-extrabold">Bienvenue sur notre plateforme d'inscription en ligne</h2>
+          <p className="text-lg leading-relaxed">
+            Vous êtes sur le point de commencer le processus d'inscription à l'Université d'Abomey-Calavi (UAC).
+            Notre page "Comment ça marche ?" est là pour vous guider à chaque étape du processus, de manière simple et claire.
+            Suivez les étapes ci-dessous pour vous inscrire avec succès.
+          </p>
         </div>
       </div>
     </section>
